ccefcec763969921b7310d0eb77ee46c1dc51b60
phiamo <phiamo@gmail.com>
2012-02-11 00:31:11 +0100

    Added more text

--- Form/Type/ExampleExtendedFormType.php
+++ Form/Type/ExampleExtendedFormType.php
@@ -11,24 +11,24 @@ class ExampleExtendedFormType extends AbstractType
     {
         $builder
             ->add('textfield1', 'text', array(
-                'label' => "Form sizes",
+                'label' => 'Form sizes',
                 'attr' => array(
                     'class' => 'span1',
-                	'placeholder' => "span1",
+                	'placeholder' => 'span1',
                 )
             ))
             ->add('textfield2', 'text', array(
                 'label_render' => false,
                 'attr' => array(
                     'class' => 'span2',
-                	'placeholder' => "span2",
+                	'placeholder' => 'span2',
                 )
             ))
             ->add('textfield3', 'text', array(
                 'label_render' => false,
                 'attr' => array(
                     'class' => 'span3',
-                	'placeholder' => "span3",
+                	'placeholder' => 'span3',
                 )
             ))
             ->add('select1', 'choice', array(
@@ -54,40 +54,40 @@ class ExampleExtendedFormType extends AbstractType
             ))
             ->add('Prepended Text', 'text', array(
                 'widget_addon' => array(
-                	'text' => "@"
+                	'text' => '@'
                 ),
                 'attr' => array(
                     'class' => 'span1',
-                	'placeholder' => "span1",
+                	'placeholder' => 'span1',
                 )
             ))
             ->add('Prepended Icon', 'text', array(
                 'widget_addon' => array(
-                	'icon' => "headphones"
+                	'icon' => 'headphones'
                 ),
                 'attr' => array(
                     'class' => 'span1',
-                	'placeholder' => "span1",
+                	'placeholder' => 'span1',
                 )
             ))
             ->add('Appended Text', 'text', array(
                 'widget_addon' => array(
-                	'text' => ".00",
+                	'text' => '.00',
                 	'append' => true
                 ),
                 'attr' => array(
                     'class' => 'span1',
-                	'placeholder' => "span1",
+                	'placeholder' => 'span1',
                 )
             ))
             ->add('Appended Icon', 'text', array(
                 'widget_addon' => array(
-                	'icon' => "pencil",
+                	'icon' => 'pencil',
                 	'append'=>true
                 ),
                 'attr' => array(
                     'class' => 'span1',
-                	'placeholder' => "span1",
+                	'placeholder' => 'span1',
                 )
             ))
             ->add('Checkboxes Inline', 'choice', array(
@@ -95,7 +95,7 @@ class ExampleExtendedFormType extends AbstractType
                 'multiple'     => true,
                 'expanded'	   => true,
                 'choices'      => array('1' => 'one', '2' => 'two', '3'=>'three'),
-                'widget_type'  => "inline"
+                'widget_type'  => 'inline'
             ))
             ->add('Checkboxes', 'choice', array(
                 'label'        => 'Checkboxes',
@@ -103,7 +103,7 @@ class ExampleExtendedFormType extends AbstractType
                 'multiple'     => true,
                 'expanded'	   => true,
                 'choices'      => array(
-                	'1' => "Option one is this and that—be sure to include why it's great",
+                	'1' => 'Option one is this and that—be sure to include why it`s great',
                  	'2' => 'Option two can also be checked and included in form results',
                  	'3' => 'Option three can—yes, you guessed it—also be checked and included in form results'
              	),
@@ -112,7 +112,7 @@ class ExampleExtendedFormType extends AbstractType
                 'label'        => 'Radio buttons',
                 'expanded'	   => true,
                 'choices'      => array(
-                	'1' => "Option one is this and that—be sure to include why it's great",
+                	'1' => 'Option one is this and that—be sure to include why it`s great',
                  	'2' => 'Option two can also be checked and included in form results',
                  	'3' => 'Option three can—yes, you guessed it—also be checked and included in form results'
              	),
