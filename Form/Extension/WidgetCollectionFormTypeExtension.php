b55d894d74fbb8173e1346c245fecdfdd8e67a48
David Desberg <david@thedesbergs.com>
2012-07-21 15:08:23 -0400

    Updated form extensions to become compatible with latest FormTypeExtensionInterface

--- Form/Extension/WidgetCollectionFormTypeExtension.php
+++ Form/Extension/WidgetCollectionFormTypeExtension.php
@@ -3,7 +3,7 @@ namespace Mopa\Bundle\BootstrapBundle\Form\Extension;
 
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\FormViewInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Exception\FormException;
 
@@ -16,7 +16,7 @@ class WidgetCollectionFormTypeExtension extends AbstractTypeExtension
         $this->options = $options;
     }
 
-    public function buildView(FormViewInterface $view, FormInterface $form, array $options)
+    public function buildView(FormView $view, FormInterface $form, array $options)
     {
         if ($options['widget_add_btn'] != null && !is_array($options['widget_add_btn'])) {
             throw new FormException('The "widget_add_btn" option must be an "array".');
