c44c7185cfa6b1d57169108ef0cc74f8ad3d6f28
antek <antek88@gmail.com>
2012-07-22 14:40:04 +0300

    Update Form/Extension/HelpFormTypeExtension.php

--- Form/Extension/HelpFormTypeExtension.php
+++ Form/Extension/HelpFormTypeExtension.php
@@ -10,7 +10,7 @@ class HelpFormTypeExtension extends AbstractTypeExtension
 {
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
-        $view->vars['inline'] = $options['help_inline'];
+        $view->vars['help_inline'] = $options['help_inline'];
         $view->vars['help_block'] = $options['help_block'];
         $view->vars['help_label'] = $options['help_label'];
     }
