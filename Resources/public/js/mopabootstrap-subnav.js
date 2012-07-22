e706f1d670af3a7ec697deac9f4c58d1a78223f3
antek <antek88@gmail.com>
2012-07-23 02:08:07 +0300

    Firefox with Firebug error reporting on line - $('.subnav').attr('data-top', offset.top); but i don't have subnav. Now is checking for subnav class.

--- Resources/public/js/mopabootstrap-subnav.js
+++ Resources/public/js/mopabootstrap-subnav.js
@@ -5,21 +5,24 @@
  */
 
  $(document).scroll(function(){
-     // If has not activated (has no attribute "data-top"
-     if (!$('.subnav').attr('data-top')) {
-         // If already fixed, then do nothing
-         if ($('.subnav').hasClass('subnav-fixed')) {
-             return;
-         }
-         // Remember top position
-         var offset = $('.subnav').offset();
-         $('.subnav').attr('data-top', offset.top);
-     }
-    
-     if ($('.subnav').attr('data-top') - $('.subnav').outerHeight() <= $(this).scrollTop()){
-         $('.subnav').addClass('subnav-fixed');
-     } else{
-         $('.subnav').removeClass('subnav-fixed');
+     //check for .subnav
+     if ($('.subnav').length) {
+       // If has not activated (has no attribute "data-top"
+       if (!$('.subnav').attr('data-top')) {
+           // If already fixed, then do nothing
+           if ($('.subnav').hasClass('subnav-fixed')) {
+               return;
+           }
+           // Remember top position
+           var offset = $('.subnav').offset();
+           $('.subnav').attr('data-top', offset.top);
+       }
+      
+       if ($('.subnav').attr('data-top') - $('.subnav').outerHeight() <= $(this).scrollTop()){
+           $('.subnav').addClass('subnav-fixed');
+       } else{
+           $('.subnav').removeClass('subnav-fixed');
+       }
      }
  });
  /*
