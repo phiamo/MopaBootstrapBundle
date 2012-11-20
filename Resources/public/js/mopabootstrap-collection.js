6116e8c3fa328561aec699c94fba14d568a696c9
Marcin Radziwoński <m.radziwonski@epro.com.pl>
2012-11-20 11:22:03 +0100

    Removed collection item parent div

--- Resources/public/js/mopabootstrap-collection.js
+++ Resources/public/js/mopabootstrap-collection.js
@@ -46,9 +46,8 @@
             this.addPrototype(index);
         },
         addPrototype: function(index) {
-            var rowContent = $(this.options.collection_id).attr('data-prototype').replace(/__name__/g, index);
-            var row = $("<div />");
-            row.html(rowContent);
+            var rowContent = $(this.options.collection_id).attr('data-prototype').replace(/__name__/g, index);            
+            var row = $(rowContent);     
             $('div' + this.options.collection_id + '> .controls').append(row);
             $(this.options.collection_id).trigger('add.mopa-collection-item', [row]);
         },
