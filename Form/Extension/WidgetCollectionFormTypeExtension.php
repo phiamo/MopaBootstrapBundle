c0b25131637ec48fa3e55b10ab9ed674dde0fb07
phiamo <phiamo@googlemail.com>
2013-04-29 20:24:48 +0200

    Replaced deprecated Exceptions to solve #423

--- Form/Extension/WidgetCollectionFormTypeExtension.php
+++ Form/Extension/WidgetCollectionFormTypeExtension.php
@@ -5,7 +5,8 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
-use Symfony\Component\Form\Exception\FormException;
+use Symfony\Component\Form\Exception\UnexpectedTypeException;
+use Symfony\Component\Form\Exception\LogicException;
 
 class WidgetCollectionFormTypeExtension extends AbstractTypeExtension
 {
@@ -19,32 +20,32 @@ class WidgetCollectionFormTypeExtension extends AbstractTypeExtension
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
         if ($options['widget_add_btn'] != null && !is_array($options['widget_add_btn'])) {
-            throw new FormException('The "widget_add_btn" option must be an "array".');
+            throw new UnexpectedTypeException($options['widget_add_btn'], "array");
         } elseif ($options['widget_add_btn'] != null) {
             if (isset($options['widget_add_btn']['attr']) && !is_array($options['widget_add_btn']['attr'])) {
-                throw new FormException('The "widget_add_btn.attr" option must be an "array".');
+                throw new UnexpectedTypeException($options['widget_add_btn'], "array");
             }
             if (!isset($options['widget_add_btn']['attr'])) {
                 $options['widget_add_btn']['attr'] = $this->options['widget_add_btn']['attr'];
             }
             if (!isset($options['widget_add_btn']['label']) && !isset($options['widget_add_btn']['icon'])) {
-                throw new FormException('Provide either "icon" or "label" to "widget_add_btn"');
+                throw new LogicException('Provide either "icon" or "label" to "widget_add_btn"');
             }
             if (!isset($options['widget_add_btn']['icon']) && $this->options['widget_add_btn']['icon'] != null) {
                 $options['widget_add_btn']['icon'] = $this->options['widget_add_btn']['icon'];
             }
         }
         if ($options['widget_remove_btn'] != null && !is_array($options['widget_remove_btn'])) {
-            throw new FormException('The "widget_remove_btn" option must be an "array".');
+                throw new UnexpectedTypeException($options['widget_remove_btn'], "array");
         } elseif ($options['widget_remove_btn'] != null) {
             if (isset($options['widget_remove_btn']) && !is_array($options['widget_remove_btn'])) {
-                throw new FormException('The "widget_remove_btn" option must be an "array".');
+                throw new UnexpectedTypeException($options['widget_remove_btn'], "array");
             }
             if (!isset($options['widget_remove_btn']['attr'])) {
                 $options['widget_remove_btn']['attr'] = $this->options['widget_remove_btn']['attr'];
             }
             if (!isset($options['widget_remove_btn']['label']) && !isset($options['widget_remove_btn']['icon'])) {
-                 throw new FormException('Provide either "icon" or "label" to "widget_remove_btn"');
+                 throw new LogicException('Provide either "icon" or "label" to "widget_remove_btn"');
             }
             if (!isset($options['widget_remove_btn']['icon']) && $this->options['widget_remove_btn']['icon'] != null) {
                 $options['widget_remove_btn']['icon'] = $this->options['widget_remove_btn']['icon'];
