4397a7f078c30525617049b22e5916da3c93cc4e
PayteR <payter86@gmail.com>
2013-08-01 02:37:41 +0200

    Bootstrap version 3 init

--- Resources/public/js/mopabootstrap-collection.js
+++ Resources/public/js/mopabootstrap-collection.js
@@ -54,7 +54,7 @@
         remove: function () {
                 if (this.$element.parents('.collection-item').length !== 0){
                     var row = this.$element.closest('.collection-item');
-                    row.remove();X
+                    row.remove();
                     $(this.options.collection_id).triggerHandler('remove.mopa-collection-item', [row]);
                 }
         }
