ab4d562317a931c006ec5a8d25036b45a3dd657d
buschjost <github.com@oliver-buschjost.de>
2013-09-11 16:14:24 +0200

    Fix SF 2.3 Exception changes
    InvalidConfigurationException is now used as FormException was removed.

--- Form/Extension/WidgetCollectionFormTypeExtension.php
+++ Form/Extension/WidgetCollectionFormTypeExtension.php
@@ -5,7 +5,7 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
-use Symfony\Component\Form\Exception\FormException;
+use Symfony\Component\Form\Exception\InvalidConfigurationException;
 
 class WidgetCollectionFormTypeExtension extends AbstractTypeExtension
 {
@@ -19,16 +19,16 @@ class WidgetCollectionFormTypeExtension extends AbstractTypeExtension
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
         if ($options['widget_add_btn'] != null && !is_array($options['widget_add_btn'])) {
-            throw new FormException('The "widget_add_btn" option must be an "array".');
+            throw new InvalidConfigurationException('The "widget_add_btn" option must be an "array".');
         } elseif ($options['widget_add_btn'] != null) {
             if (isset($options['widget_add_btn']['attr']) && !is_array($options['widget_add_btn']['attr'])) {
-                throw new FormException('The "widget_add_btn.attr" option must be an "array".');
+                throw new InvalidConfigurationException('The "widget_add_btn.attr" option must be an "array".');
             }
             if (!isset($options['widget_add_btn']['attr'])) {
                 $options['widget_add_btn']['attr'] = $this->options['widget_add_btn']['attr'];
             }
             if (!isset($options['widget_add_btn']['label']) && !isset($options['widget_add_btn']['icon'])) {
-                throw new FormException('Provide either "icon" or "label" to "widget_add_btn"');
+                throw new InvalidConfigurationException('Provide either "icon" or "label" to "widget_add_btn"');
             }
             if (!isset($options['widget_add_btn']['icon']) && $this->options['widget_add_btn']['icon'] != null) {
                 $options['widget_add_btn']['icon'] = $this->options['widget_add_btn']['icon'];
@@ -41,13 +41,13 @@ class WidgetCollectionFormTypeExtension extends AbstractTypeExtension
             throw new FormException('The "widget_remove_btn" option must be an "array".');
         } elseif ($options['widget_remove_btn'] != null) {
             if (isset($options['widget_remove_btn']) && !is_array($options['widget_remove_btn'])) {
-                throw new FormException('The "widget_remove_btn" option must be an "array".');
+                throw new InvalidConfigurationException('The "widget_remove_btn" option must be an "array".');
             }
             if (!isset($options['widget_remove_btn']['attr'])) {
                 $options['widget_remove_btn']['attr'] = $this->options['widget_remove_btn']['attr'];
             }
             if (!isset($options['widget_remove_btn']['label']) && !isset($options['widget_remove_btn']['icon'])) {
-                 throw new FormException('Provide either "icon" or "label" to "widget_remove_btn"');
+                 throw new InvalidConfigurationException('Provide either "icon" or "label" to "widget_remove_btn"');
             }
             if (!isset($options['widget_remove_btn']['icon']) && $this->options['widget_remove_btn']['icon'] != null) {
                 $options['widget_remove_btn']['icon'] = $this->options['widget_remove_btn']['icon'];
