011eb6e3517a2b36bee5d109e21977b95b4f45f1
Alan Doucette <alan@juntacorp.com>
2013-09-25 14:02:56 -0400

    Split widget_addon to widget_addon_prepend and widget_addon_append. Patch converted from PR #534.

--- Form/Extension/WidgetFormTypeExtension.php
+++ Form/Extension/WidgetFormTypeExtension.php
@@ -5,7 +5,7 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
-use Symfony\Component\Form\Exception\CreationException;
+use Symfony\Component\Form\Exception\InvalidConfigurationException;
 
 class WidgetFormTypeExtension extends AbstractTypeExtension
 {
@@ -18,26 +18,26 @@ class WidgetFormTypeExtension extends AbstractTypeExtension
 
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
-        if (!is_array($options['widget_addon'])) {
-            throw new CreationException("The 'widget_addon' option must be an array");
+        if (null !== $options['widget_addon_prepend'] && !is_array($options['widget_addon_prepend'])) {
+            throw new InvalidConfigurationException("The 'widget_addon_prepend' option must be an array");
+        }
+        if (null !== $options['widget_addon_append'] && !is_array($options['widget_addon_append'])) {
+            throw new InvalidConfigurationException("The 'widget_addon_append' option must be an array");
         }
         if (in_array('percent', $view->vars['block_prefixes'])) {
-            if ($options['widget_addon']['type'] === null) {
-                $options['widget_addon']['type'] = 'append';
+            if ($options['widget_addon_append'] === null) {
+                $options['widget_addon_append'] = array();
             }
         }
         if (in_array('money', $view->vars['block_prefixes'])) {
-            if ($options['widget_addon']['type'] === null) {
-                $options['widget_addon']['type'] = 'prepend';
+            if ($options['widget_addon_prepend'] === null) {
+                $options['widget_addon_prepend'] = array();
             }
-        }
-        if (((isset($options['widget_addon']['text']) && $options['widget_addon']['text'] !== null)
-                || (isset($options['widget_addon']['icon']) && $options['widget_addon']['icon'] !== null)) && $options['widget_addon']['type'] === null) {
-            throw new \Exception('You must provide a "type" for widget_addon');
         }
 
         $view->vars['widget_form_group'] = $options['widget_form_group'];
-        $view->vars['widget_addon'] = $options['widget_addon'];
+        $view->vars['widget_addon_prepend'] = $options['widget_addon_prepend'];
+        $view->vars['widget_addon_append'] = $options['widget_addon_append'];
         $view->vars['widget_prefix'] = $options['widget_prefix'];
         $view->vars['widget_suffix'] = $options['widget_suffix'];
         $view->vars['widget_type'] = $options['widget_type'];
@@ -51,11 +51,8 @@ class WidgetFormTypeExtension extends AbstractTypeExtension
         $resolver->setDefaults(
             array(
                 'widget_form_group' => true,
-                'widget_addon' => array(
-                    'type' => null, //false: dont add anything, null: using presets, anything; prepend; append
-                    'icon' => null,
-                    'text' => null,
-                ),
+                'widget_addon_prepend' => null,
+                'widget_addon_append' => null,
                 'widget_prefix' => null,
                 'widget_suffix' => null,
                 'widget_type' => '',
