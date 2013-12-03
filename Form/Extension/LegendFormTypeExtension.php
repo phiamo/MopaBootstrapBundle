159eff532ceb6f9143df88234683772bf11f2e45
Philipp A. Mohrenweiser <phiamo@googlemail.com>
2013-12-03 01:07:18 +0100

    Added feature to change legend tag for child forms/collections

--- Form/Extension/LegendFormTypeExtension.php
+++ Form/Extension/LegendFormTypeExtension.php
@@ -17,6 +17,7 @@ class LegendFormTypeExtension extends AbstractTypeExtension
     private $renderFieldset;
     private $showLegend;
     private $showChildLegend;
+    private $legendTag;
     private $renderRequiredAsterisk;
     private $renderOptionalText;
 
@@ -30,6 +31,7 @@ class LegendFormTypeExtension extends AbstractTypeExtension
         $this->renderFieldset = $options['render_fieldset'];
         $this->showLegend = $options['show_legend'];
         $this->showChildLegend = $options['show_child_legend'];
+        $this->legendTag = $options['legend_tag'];
         $this->renderRequiredAsterisk = $options['render_required_asterisk'];
         $this->renderOptionalText = $options['render_optional_text'];
     }
@@ -42,6 +44,7 @@ class LegendFormTypeExtension extends AbstractTypeExtension
         $view->vars['render_fieldset'] = $options['render_fieldset'];
         $view->vars['show_legend'] = $options['show_legend'];
         $view->vars['show_child_legend'] = $options['show_child_legend'];
+        $view->vars['legend_tag'] = $options['legend_tag'];
         $view->vars['label_render'] = $options['label_render'];
         $view->vars['render_required_asterisk'] = $options['render_required_asterisk'];
         $view->vars['render_optional_text'] = $options['render_optional_text'];
@@ -56,6 +59,7 @@ class LegendFormTypeExtension extends AbstractTypeExtension
             'render_fieldset' => $this->renderFieldset,
             'show_legend' => $this->showLegend,
             'show_child_legend' => $this->showChildLegend,
+            'legend_tag' => $this->legendTag,
             'label_render' => true,
             'render_required_asterisk' => $this->renderRequiredAsterisk,
             'render_optional_text' => $this->renderOptionalText,
