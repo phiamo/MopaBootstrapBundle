d691ca78d345fb73993793a4b777ac22b5e254fc
peshi <peshis@gmail.com>
2014-01-31 14:31:10 +0100

    Ability to use form-control-static

---
+++ Form/Extension/StaticTextExtension.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the MopaBootstrapBundle.
+ *
+ * (c) Philipp A. Mohrenweiser <phiamo@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mopa\Bundle\BootstrapBundle\Form\Extension;
+
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+
+/**
+ * Extension for creating static text (form-control-static)
+ *
+ * @author peshi <peshis@gmail.com>
+ */
+class StaticTextExtension extends AbstractTypeExtension
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExtendedType()
+    {
+        return 'text';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'static_text' => null,
+            )
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['static_text'] = $options['static_text'];
+
+    }
+}
