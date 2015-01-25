7102e21a0848d63b121b00c8a22ad7383336837a
Steffen Roßkamp <steffen.rosskamp@gimmickmedia.de>
2015-01-25 16:58:20 +0100

    Add button addons for input groups

--- Form/Extension/WidgetFormTypeExtension.php
+++ Form/Extension/WidgetFormTypeExtension.php
@@ -55,6 +55,8 @@ class WidgetFormTypeExtension extends AbstractTypeExtension
         $view->vars['widget_form_group'] = $options['widget_form_group'];
         $view->vars['widget_addon_prepend'] = $options['widget_addon_prepend'];
         $view->vars['widget_addon_append'] = $options['widget_addon_append'];
+        $view->vars['widget_btn_prepend'] = $options['widget_btn_prepend'];
+        $view->vars['widget_btn_append'] = $options['widget_btn_append'];
         $view->vars['widget_prefix'] = $options['widget_prefix'];
         $view->vars['widget_suffix'] = $options['widget_suffix'];
         $view->vars['widget_type'] = $options['widget_type'];
@@ -73,6 +75,8 @@ class WidgetFormTypeExtension extends AbstractTypeExtension
             'widget_form_group' => true,
             'widget_addon_prepend' => null,
             'widget_addon_append' => null,
+            'widget_btn_prepend' => null,
+            'widget_btn_append' => null,
             'widget_prefix' => null,
             'widget_suffix' => null,
             'widget_type' => '',
