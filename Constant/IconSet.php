5f52559fa80e6bca6743b239d5354aa9e9225764
Christian Hessel <c.hessel@opwoco.de>
2015-02-16 19:22:59 +0100

    added config for multiple icon support

---
+++ Constant/IconSet.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace opwoco\Bundle\BootstrapBundle\Constant;
+
+
+class IconSet
+{
+    const GLYPHICON = 'glyphicon';
+    const FONTAWESOME = 'fontawesome';
+    const FOUNDATION = 'foundation';
+    const IONICONS = 'ionicons';
+    const OCTICONS = 'octicons';
+
+}
