3a39ea8798753c36f62532e504cf5a7344c58385
Christian Hessel <c.hessel@opwoco.de>
2015-02-25 15:53:18 +0100

    added config for help-block wrapper

--- Form/Extension/HelpFormTypeExtension.php
+++ Form/Extension/HelpFormTypeExtension.php
@@ -40,11 +40,16 @@ class HelpFormTypeExtension extends AbstractTypeExtension
      */
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
+
         $view->vars['help_block'] = $options['help_block'];
         $view->vars['help_label'] = $options['help_label'];
 
         if (null !== $options['help_label_tooltip'] && !is_array($options['help_label_tooltip'])) {
             throw new InvalidArgumentException('The "help_label_tooltip" option must be an "array".');
+        }
+
+        if ($options['help_block_horizontal_wrapper_class']) {
+            $options['help_block_horizontal_wrapper_class'] = $this->options['help_block_horizontal_wrapper_class'];
         }
 
         if ($options['help_label_tooltip']) {
@@ -150,6 +155,7 @@ class HelpFormTypeExtension extends AbstractTypeExtension
             }
         }
 
+        $view->vars['help_block_horizontal_wrapper_class'] = $options['help_block_horizontal_wrapper_class'];
         $view->vars['help_label_tooltip'] = $options['help_label_tooltip'];
         $view->vars['help_label_popover'] = $options['help_label_popover'];
         $view->vars['help_block_tooltip'] = $options['help_block_tooltip'];
@@ -165,6 +171,7 @@ class HelpFormTypeExtension extends AbstractTypeExtension
         $resolver->setDefaults(array(
             'help_block' => null,
             'help_label' => null,
+            'help_block_horizontal_wrapper_class' => $this->options['help_block_horizontal_wrapper_class'],
             'help_label_tooltip' => $this->options['help_label_tooltip'],
             'help_label_popover' => $this->options['help_label_popover'],
             'help_block_tooltip' => $this->options['help_block_tooltip'],
