23ff5d8dd5d79ff6571e62a02d1e349da4141a9d
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-03-09 12:13:37 +0000

    Datepicker now works for fields added after page initialisation

--- Resources/public/js/mopabootstrap-datepicker.js
+++ Resources/public/js/mopabootstrap-datepicker.js
@@ -1,33 +1,33 @@
 (function ($) {
+
+    var datepickerOptions = {
+        autoclose     : true,
+        format        : 'dd/mm/yyyy',
+        language      : 'fr',
+        minView       : 'month',
+        pickerPosition: 'bottom-left',
+        todayBtn      : true,
+        startView     : 'month'
+    };
+    var datetimepickerOptions = {
+        autoclose     : true,
+        format        : 'dd/mm/yyyy hh:ii',
+        language      : 'fr',
+        pickerPosition: 'bottom-left',
+        todayBtn      : true
+    };
+    var timepickerOptions = {
+        autoclose     : true,
+        format        : 'hh:ii',
+        formatViewType: 'time',
+        maxView       : 'day',
+        minView       : 'hour',
+        pickerPosition: 'bottom-left',
+        startView     : 'day'
+    };
+
+
     $(function () {
-
-        $('[data-provider="datepicker"]').datetimepicker({
-            autoclose     : true,
-            format        : 'dd/mm/yyyy',
-            language      : 'fr',
-            minView       : 'month',
-            pickerPosition: 'bottom-left',
-            todayBtn      : true,
-            startView     : 'month'
-        });
-
-        $('[data-provider="datetimepicker"]').datetimepicker({
-            autoclose     : true,
-            format        : 'dd/mm/yyyy hh:ii',
-            language      : 'fr',
-            pickerPosition: 'bottom-left',
-            todayBtn      : true
-        });
-
-        $('[data-provider="timepicker"]').datetimepicker({
-            autoclose     : true,
-            format        : 'hh:ii',
-            formatViewType: 'time',
-            maxView       : 'day',
-            minView       : 'hour',
-            pickerPosition: 'bottom-left',
-            startView     : 'day'
-        });
 
         // Restore value from hidden input
         $('input[type=hidden]', '.date').each(function () {
@@ -37,4 +37,40 @@
         });
 
     });
+
+    $(document)
+        .on(
+        'focus.mopa.datetimepicker.data-api click.datetimepicker.data-api',
+        '[data-provider="datepicker"]',
+        function (e) {
+            var $this = $(this);
+            if ($this.data('datetimepicker')) return;
+            e.preventDefault();
+            // component click requires us to explicitly show it
+            $this.datetimepicker(datepickerOptions);
+        }
+    )
+        .on(
+        'focus.mopa.datetimepicker.data-api click.datetimepicker.data-api',
+        '[data-provider="datetimepicker"]',
+        function (e) {
+            var $this = $(this);
+            if ($this.data('datetimepicker')) return;
+            e.preventDefault();
+            // component click requires us to explicitly show it
+            $this.datetimepicker(datetimepickerOptions);
+        }
+    )
+        .on(
+        'focus.mopa.datetimepicker.data-api click.datetimepicker.data-api',
+        '[data-provider="timepicker"]',
+        function (e) {
+            var $this = $(this);
+            if ($this.data('datetimepicker')) return;
+            e.preventDefault();
+            // component click requires us to explicitly show it
+            $this.datetimepicker(timepickerOptions);
+        }
+    );
+
 })(jQuery);
